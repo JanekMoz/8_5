6db70c39b398501f8e45f1faa14385852d5cca2a
JanekMoz <mozyr12@gmail.com>
2017-01-08 20:21:41 +0100

    Add files via upload

---
+++ Zadanie_8_5.js
@@ -0,0 +1,20 @@
+var (a*a) + (2 * a * b) - (b*b) = value; 
+if value > 1 {
+	'wynik dodatni'
+	console.log('Okno z gratulacjami')
+}
+else if value < 1 {
+	'wynik ujemy'
+	console.log('Zagraj melodyjkę')
+}
+else value = 0 {
+	'błąd'
+	console.log('Zmień kolor strony na czrny')
+}
+if ( 0 == 0 ) {
+    'zero'
+    console.log('Zmień kolor strony na czerwony')
+}
+var a = 1, 
+	b = a + 1;
+	
