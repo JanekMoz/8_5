ac5ac6cf2e9ac9fafd34cc37ce9cd3b95c82293a
JanekMoz <mozyr12@gmail.com>
2017-01-10 19:27:31 +0100

    Update Zadanie_8_5.js
    Wprowadzilem poprawki. Czy o to chodzi?

--- Zadanie_8_5.js
+++ Zadanie_8_5.js
@@ -1,17 +1,17 @@
 var (a*a) + (2 * a * b) - (b*b) = value; 
-if value > 1 {
+if (value) > 1 {
 	'wynik dodatni'
 	console.log('Okno z gratulacjami')
 }
-else if value < 1 {
+else if (value) < 1 {
 	'wynik ujemy'
 	console.log('Zagraj melodyjkę')
 }
-else value = 0 {
+else (value) = 0 {
 	'błąd'
-	console.log('Zmień kolor strony na czrny')
+	console.log('Zmień kolor strony na czarny')
 }
-if ( 0 == 0 ) {
+else if ( value == 0 ) {
     'zero'
     console.log('Zmień kolor strony na czerwony')
 }
